43002cd908d5085c841e2cb18475fdb91f4bc97e
NagorniyAleksandr <Aleksandr.Nagornyy@it-integrator.ua>
2016-10-28 17:51:35 +0300

    done toJson

---
+++ src/test/java/parser/FromJSONConverterComplexObjTest.java
@@ -0,0 +1,16 @@
+package parser;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by nagornyyalek on 28.10.2016.
+ */
+public class FromJSONConverterComplexObjTest {
+    @Test
+    public void fromJson() throws Exception {
+
+    }
+
+}
---
+++ src/test/java/parser/FromJSONConverterJavaLangObjTest.java
@@ -0,0 +1,16 @@
+package parser;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by nagornyyalek on 28.10.2016.
+ */
+public class FromJSONConverterJavaLangObjTest {
+    @Test
+    public void fromJson() throws Exception {
+
+    }
+
+}
